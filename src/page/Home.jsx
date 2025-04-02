0d09b800527cb3f9d121fc64d215ca4e4f992e75
Theo Stoffelbach <theo.stoffelbach@ynov.com>
2025-04-02 21:38:15 +0200

    Test deployement #7

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../style/_allpages.sass";
 import ModalFormHome from "../Component/modal/ModalFormHome";
-import NavBar from "../Component/other/NavBar";
+// import NavBar from "../Component/other/NavBar";
 
 function Home() {
     return (
