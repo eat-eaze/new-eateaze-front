89e90bab065838e7b62f85d2c937c65b637babba
Theo Stoffelbach <theo.stoffelbach@ynov.com>
2025-05-18 15:54:03 +0200

    Ajout de la configuration fetch par défaut dans config.js et mise à jour de l'appel API dans CardCustomerGrid pour utiliser cette configuration. Cela améliore la gestion des requêtes API en ajoutant des options CORS et des en-têtes personnalisés.

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,19 @@ export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "https://api.tousalaferme.theo-stoffelbach.fr";
 
 // URL complète de l'API incluant le chemin /api
+// Utiliser un proxy CORS si nécessaire
+// export const API_URL = `https://cors-anywhere.herokuapp.com/${API_BASE_URL}/api`;
 export const API_URL = `${API_BASE_URL}/api`;
+
+// Configuration fetch par défaut pour les requêtes API
+export const fetchConfig = {
+  mode: 'cors',
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json',
+    // Ajout d'en-têtes personnalisés si nécessaire
+  }
+};
 
 // Autres constantes globales peuvent être ajoutées ici
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
